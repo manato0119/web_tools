f0826e454fd4099953a17a06db966e89e594b311
manato0119 <m.ishiwata0119@gmail.com>
2019-10-29 14:14:12 +0900

    [修正]キャプチャ取得 sp対応

--- get_capture.php
+++ get_capture.php
@@ -4,6 +4,8 @@
 
     use Spatie\Browsershot\Browsershot;
 
+    $config['device'] = 'pc';
+
     $url_list_file = 'list.txt';
     $out_image_dir = 'img';
 
@@ -14,10 +16,22 @@
 
         if (empty($url)) continue;
 
-        $filename = ($key + 1) . '.png';
+        $filename = $config['device'] . '_' . ($key + 1) . '.png';
 
-        Browsershot::url($url)
-            ->fullPage()
-            ->save($out_image_dir. '/' . $filename);
+        if ($config['device'] == 'pc') {
+            Browsershot::url($url)
+                ->fullPage()
+                ->save($out_image_dir. '/' . $filename);
+        } else {
+            Browsershot::url($url)
+                ->userAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1')
+                ->windowSize(375, 812)
+                ->deviceScaleFactor(3)
+                ->mobile()
+                ->touch()
+                ->landscape(false)
+                ->fullPage()
+                ->save($out_image_dir. '/' . $filename);
+        }
 
     }
